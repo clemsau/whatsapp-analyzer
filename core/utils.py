425ecbb8e79073a3b1590fb743bf4e84eac201da
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-05-25 21:17:29 +0200

    Add sharing thubnail

--- core/utils.py
+++ core/utils.py
@@ -4,6 +4,7 @@ import re
 import random
 import emoji
 import datetime
+import time
 
 
 from .conf.settings import UPLOAD_FOLDER
@@ -134,15 +135,19 @@ def dataframe_insight(dataframe):
 
 
 def conversation_users(df):
+    time1 = time.time()
     users = []
     df = df.groupby('Author').count().reset_index()
     for index, row in df.iterrows():
         users.append(row['Author'])
+    print("- Conversation users: {}".format(time.time() - time1))
     return users
 
 
 def user_number(df):
+    time1 = time.time()
     df = df.groupby('Author').count()
+    print("- User number: {}".format(time.time() - time1))
     return len(df)
 
 
@@ -153,21 +158,26 @@ def color_set(color_number):
 
 
 def total_word_count(df):
+    time1 = time.time()
     total_words = df['Message'].str.lower().str.split().apply(lambda l: len(l)).sum()
+    print("- Total word count: {}".format(time.time() - time1))
     return total_words
 
 
 def messages_by_user(df):
+    time1 = time.time()
     users, message_count = [], []
     df = df.groupby('Author').count().reset_index()
     for index, row in df.iterrows():
         users.append(row['Author'])
         message_count.append(row['Message'])
+    print("- Messages per user: {}".format(time.time() - time1))
     return {"users": users,
             "message_count": message_count}
 
 
 def average_message_length_per_user(df):
+    time1 = time.time()
     users, average_message_length = [], []
     _df = df.copy()
     _df['Message'] = _df['Message'].apply(lambda s: len(s.split()))
@@ -175,11 +185,13 @@ def average_message_length_per_user(df):
     for index, row in _df.iterrows():
         users.append(row['Author'])
         average_message_length.append(row['Message'])
+    print("- Average message length per user: {}".format(time.time() - time1))
     return {"users": users,
             "message_length": average_message_length}
 
 
 def most_common_emojis(df):
+    time1 = time.time()
     word_counts_df = df.Message.str.split(expand=True).stack().value_counts()
     top_emojis, top_counts = [], []
     for index, row in word_counts_df.iteritems():
@@ -188,12 +200,29 @@ def most_common_emojis(df):
             top_counts.append(row)
         if len(top_emojis) > 4:
             break
+    print("- Most common emojis: {}".format(time.time() - time1))
     return {"top_emojis": top_emojis,
             "top_counts": top_counts,
             "colors": color_set(len(top_emojis))}
 
 
+def count_emojis(s):
+    count = 0
+    for c in s:
+        if c in emoji.EMOJI_UNICODE:
+            count += 1
+    return count
+
+
+def count_the_emojis(df):
+    _df = df.copy()
+    _df['Message'] = _df['Message'].apply(lambda s: count_emojis(s))
+    return _df['Message_']
+
+
+
 def number_of_message_per_day(df):
+    time1 = time.time()
     df = df.groupby('Date').count().reset_index()
     dates_range = list(date_range(df['Date'].min(), df['Date'].max()))
     number_dates = len(dates_range)
@@ -201,6 +230,7 @@ def number_of_message_per_day(df):
     for index, row in df.iterrows():
         message_count[dates_range.index(row['Date'])] = row['Message']
     dates_range = list(map(lambda d: d.strftime('%d/%m/%Y'), dates_range))
+    print("- Number of message per day: {}".format(time.time() - time1))
     return {"days": dates_range,
             "message_count": message_count}
 
@@ -211,9 +241,11 @@ def compute_age(start_date):
 
 
 def messages_by_days_of_the_week(df):
+    time1 = time.time()
     df = df['Date'].apply(lambda d: d.weekday()).reset_index().groupby('Date').count()
     week_days, week_days_message_count = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], [0]*7
     for index, row in df.iterrows():
         week_days_message_count[index-1] = row[0]
+    print("- Message by day of the week: {}".format(time.time()-time1))
     return {'week_days': week_days,
             'week_days_message_count': week_days_message_count}
